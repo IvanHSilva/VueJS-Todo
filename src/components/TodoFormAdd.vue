13d014b1cfc48c94558c2e932f690e44f2f71239
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2026-07-16 16:48:36 -0300

    Layout minor adjustments

--- src/components/TodoFormAdd.vue
+++ src/components/TodoFormAdd.vue
@@ -1,5 +1,5 @@
 <template>
-  <form @submit.prevent="addItem" class="flex items-center px-4 bg-gray-900 h-15 rounded-sm border-l-2
+  <form @submit.prevent="addItem" class="flex items-center px-4 bg-gray-900 h-16 rounded-sm border-l-2
     border-green-400 mb-3">
     <input v-model="newItem" placeholder="Adicione um novo item..." type="text" class="bg-gray-900 placeholder-gray-500 text-gray-500 font-light
       focus:outline-none block w-full appearance-none leading-normal py-3 pr-3" />
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen bg-gray-800">
     <!-- Content -->
     <div class="px-3 py-10 md:px-10">
-      <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+      <div class="w-full max-w-xl mx-auto">
 
         <TodoSpinner v-if="loading" />
 
